50ada03fd2d5df2f216f457572153dbe8c015583
Leo Wu <Leo.Wu@amd.com>
2025-04-25 06:57:50 +0000

    mount folder

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,7 @@
+MOUNT_DIR=/home/leowu102/git
+
 docker run \
+    --mount type=bind,source="$MOUNT_DIR",target=/app/git \
     --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined \
     --rm -it \
     compute-artifactory.amd.com:5000/rocm-plus-docker/compute-rocm-dkms-no-npi-hipclang:15643-ubuntu-22.04
