25cef09121c4c42abd76c05bce1443b063901f38
Leo Wu <Leo.Wu@amd.com>
2025-04-25 11:38:17 +0000

    add exec to connect to an existing container

---
+++ docker/exec.sh
@@ -0,0 +1,3 @@
+CONTAINER_NAME=amd_onboard
+
+docker exec -it $CONTAINER_NAME bash
--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,9 @@
 MOUNT_DIR=/home/leowu102/git
+CONTAINER_NAME=amd_onboard
 
 docker run \
     --mount type=bind,source="$MOUNT_DIR",target=/app/git \
     --device /dev/kfd --device /dev/dri --security-opt seccomp=unconfined \
+    --name $CONTAINER_NAME \
     --rm -it \
     compute-artifactory.amd.com:5000/rocm-plus-docker/compute-rocm-dkms-no-npi-hipclang:15643-ubuntu-22.04
